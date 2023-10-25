80d194b7c6c4ff54b04d993b4a18d70e43ba3682
shawnconway <shawnway210@gmail.com>
2023-10-25 14:23:34 -0400

    serialization

--- server/models.py
+++ server/models.py
@@ -7,12 +7,14 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 
-class Pet(db.Model):
+class Pet(db.Model, SerializerMixin):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+
 
     def __repr__(self):
         return f'<Pet {self.id}, {self.name}, {self.species}>'
